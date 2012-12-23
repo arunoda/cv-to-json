f7999d884886f0470fdc2de5507dedf3c792d7e4
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2012-12-23 23:52:14 +0530

    upload-cv changed to access 3rd param as the host

--- upload-cv.sh
+++ upload-cv.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-PORT=11111
 PDF=$1
 EXTRACT_FIELDS=$2
+PORT=11111
+HOST=$3
 
-curl http://localhost:$PORT -T $1 -H "x-extract-fields: $EXTRACT_FIELDS"
+if [ -z "$HOST" ]; then
+	HOST="localhost"
+fi
+
+curl http://$HOST:$PORT -T $1 -H "x-extract-fields: $EXTRACT_FIELDS"
